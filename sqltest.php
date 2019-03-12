a069cfc4d978e02dbe42a7e45a6cb57d1b2180ec
natanaelwidiyanto <48471358+natanaelwidiyanto@users.noreply.github.com>
2019-03-12 16:17:06 +0700

    sqltest.php

---
+++ sqltest.php
@@ -0,0 +1,22 @@
+<?php
+    $serverName = "sqlserverphp.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "mySampleDatabase", // update me
+        "Uid" => "adminsql", // update me
+        "PWD" => "P@ssw0rd_2019" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
